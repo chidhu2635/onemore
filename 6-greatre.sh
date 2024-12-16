7ab01d246ecfbb8b89d883999049c35fdc5b6ad6
chidu <chidu2635@gmail.com>
2024-12-16 16:46:53 +0530

    if condition

--- 6-greatre.sh
+++ 6-greatre.sh
@@ -2,9 +2,9 @@
 
 num1=$1
 
-if [ $num1 -eq 10 ]
+if [ $num1 -ge 10 ]
 then
-echo "entered number is greater than 0"
+echo "entered number is greater than 10"
 else
-echo "entered number is equl to 0"
+echo "entered number is not "
 fi
