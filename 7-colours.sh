4fcce758d2d78568006d35b9a5c60e0d5d25b29b
chidu <chidu2635@gmail.com>
2024-12-17 16:02:51 +0530

    colour

---
+++ 7-colours.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+R="\e[31m]"
+G="\e[32m]"
+N="\e[0m]"
+O="\e[33m]"
+P="\e[01m]"
+
+echo -e "This $R clolour"
+echo -e "This $G clolour"
+echo -e "This $N clolour"
+echo -e "This $O clolour"
+echo -e "This $P clolour"
