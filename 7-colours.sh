943bf2daf01321602ba3ada5e92563d7f50bdab3
chidu <chidu2635@gmail.com>
2024-12-17 16:05:17 +0530

    colour

--- 7-colours.sh
+++ 7-colours.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-R="\e[31m]"
-G="\e[32m]"
-N="\e[0m]"
-O="\e[33m]"
-P="\e[01m]"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+O="\e[33m"
+P="\e[01m"
 
 echo -e "This $R clolour, till $N this is normal"
 echo -e "This $G clolour, till $N this is normal""
