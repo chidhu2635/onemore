00c52725e27c8f2c4d396376a0124c5029ea605e
chidu <chidu2635@gmail.com>
2025-01-14 23:36:18 +0530

    all packages

--- 9-install-pack.sh
+++ 9-install-pack.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-echo "hello"
+USERID=$(id -u)
+R="\e[31m"
+N="\e[0m"
+Y="\e[33m"
 
-read lash
+if [ $USERID -ne 0 ];
+then
+echo -e "$R Error : Please Run this script with root access $N"
+exit 1;
+fi
 
-echo "$lash"
+for i in $@
+do
+yum install $i -y
+done
+
+
