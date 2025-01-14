0d383d004ff99ae2be231c75b3fc13f895f73ffc
chidu <chidu2635@gmail.com>
2025-01-15 00:01:59 +0530

    all packages

--- 9-install-pack.sh
+++ 9-install-pack.sh
@@ -6,6 +6,7 @@ SCRIPT_NAME=$0
 LOGFILE=$LOGDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
+G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
@@ -15,6 +16,16 @@ echo -e "$R Error : Please Run this script with root access $N"
 exit 1;
 fi
 
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+    echo -e "Installing $2 ... $R Failure $N"
+    exit 1
+    else
+    echo -e "Installing $2 ... $G Success $N"
+    fi
+}
+
 for i in $@
 do
 yum list installed $i &>>$LOGFILE
@@ -22,6 +33,7 @@ if [ $? -ne 0 ]
 then
 echo "$i is not installed , lets install it"
 yum install $i -y &>>$LOGFILE
+VALIDATE $? "$i"
 else
 echo -e "$Y $i is already installed $N"
 fi
